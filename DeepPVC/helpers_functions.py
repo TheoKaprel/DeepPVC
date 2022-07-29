eeac8cde386fc08d4b07bf94c825fb13a6eafff0
TheoKaprel <theo.kaprelian@creatis.insa-lyon.fr>
2022-07-29 16:00:47 +0200

    mse in helpers_functions

---
+++ DeepPVC/helpers_functions.py
@@ -0,0 +1,27 @@
+import torch
+
+from . import helpers_data
+
+
+
+
+
+def mean_square_error(dataset_loader, model):
+    params = model.params
+    MSE = 0
+    with torch.no_grad():
+        for test_it, batch in enumerate(dataset_loader):
+            model.input_data(batch)
+            fakePVfree = model.Generator(model.truePVE)
+
+            denormalized_target = helpers_data.denormalize(model.truePVfree,
+                                                           normtype=params['data_normalisation'], norm=params['norm'],
+                                                           to_numpy=False)
+            denormalized_output = helpers_data.denormalize(fakePVfree, normtype=params['data_normalisation'],
+                                                           norm=params['norm'], to_numpy=False)
+
+            norm = torch.sum(denormalized_target ** 2, dim=(1, 2, 3))
+
+            MSE += torch.sum(torch.sum((denormalized_output - denormalized_target) ** 2, dim=(1, 2, 3)) / norm ) / params['nb_testing_data']
+
+    return MSE
