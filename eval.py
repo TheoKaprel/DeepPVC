a4f6f1c1ccb7b8de1fccf131c78cb7d75e8f99f4
TheoKaprel <theo.kaprelian@creatis.insa-lyon.fr>
2023-02-01 17:33:27 +0100

    welcome back eval.py --error

--- eval.py
+++ eval.py
@@ -23,7 +23,7 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 def eval_click(pth, input, n, dataset_path,type,merged, ref, error, plot, verbose,param_comp):
 
     if error:
-        eval_error(pth, input, dataset_path,type, ref, verbose)
+        eval_error(pth, input, dataset_path,type,merged,ref, verbose,param_comp)
     if plot:
         eval_plot(pth, input, n, dataset_path,type,merged, ref, verbose,param_comp)
 
@@ -43,40 +43,46 @@ def add_or_modify_error(dataset_path, params, error_ref, error_val):
     return params
 
 
-def eval_error(lpth, input,dataset_path,type,ref, verbose):
+def eval_error(lpth, input,dataset_path,type,merged,ref, verbose,param_comp):
     device = helpers.get_auto_device("cuda")
 
-    dict_mse,dict_std_mse = {},{}
-    dict_mae,dict_std_mae = {},{}
+    dict_mse = {}
+    dict_mae = {}
 
     for pth in lpth:
         pth_file = torch.load(pth, map_location=device)
 
         params = pth_file['params']
-        pth_ref = params['ref']
+        pth_ref=params['ref']
+        legend=param_comp if (param_comp is not None) else pth_ref
+        params['jean_zay']=False
 
         model = Models.ModelInstance(params=params, from_pth=pth, resume_training=False)
+        model.load_model(pth_path=pth)
         model.switch_device(device)
         model.switch_eval()
 
         if input:
-            test_dataset = torch.tensor(helpers_data.load_image(filename=input,is_ref=ref,type = type, nb_channels=params['input_channels'],noisy=params['with_noise']),device=device)
+            test_dataset = torch.tensor(helpers_data.load_image(filename=input,is_ref=ref,type = type,params=params))
         elif dataset_path:
-            test_dataset = dataset.CustomPVEProjectionsDataset(params=params, paths=[dataset_path], test=True)
+            params['store_dataset']=True
+            params['max_nb_data']=-1
+            test_dataset = dataset.CustomPVEProjectionsDataset(params=params, paths=[dataset_path],
+                                                               test=True,filetype=type,merged=merged)
         else:
             print('ERROR : no input nor dataset specified. You need to specify EITHER a --input /path/to/input OR a number -n 10 of image to select randomly in the dataset')
             exit(0)
 
-        test_dataloader = DataLoader(dataset=test_dataset,batch_size=16,shuffle=False)
+        test_dataloader = DataLoader(dataset=test_dataset,batch_size=32,shuffle=False)
         with torch.no_grad():
-            (MNRMSE,std_NRMSE), (MNMAE,std_NMAE) = helpers_functions.validation_errors(test_dataloader=test_dataloader,model=model,do_NMAE=True, do_NRMSE=True)
-            print(f'Mean NRMSE : '+ "{:.3e}".format(MNRMSE) + "  (std={:.3e})".format(std_NRMSE))
-            print(f'Mean MNMAE : '+ "{:.3e}".format(MNMAE)  + "  (std={:.3e})".format(std_NMAE))
+            # (MNRMSE,std_NRMSE), (MNMAE,std_NMAE) = helpers_functions.validation_errors(test_dataloader=test_dataloader,model=model,do_NMAE=True, do_NRMSE=True)
+            MNRMSE, MNMAE = helpers_functions.validation_errors(test_dataloader=test_dataloader,model=model,do_NMAE=True, do_NRMSE=True)
+            MNRMSE,MNMAE=MNRMSE.item(),MNMAE.item()
+            print(f'Mean NRMSE : '+ "{:.3e}".format(MNRMSE))
+            print(f'Mean MNMAE : '+ "{:.3e}".format(MNMAE))
 
             dict_mse[pth_ref] = MNRMSE
             dict_mae[pth_ref] = MNMAE
-            dict_std_mse[pth_ref] = std_NRMSE
-            dict_std_mae[pth_ref] = std_NMAE
 
             model.params = add_or_modify_error(dataset_path=dataset_path, params=model.params, error_ref='MNRMSE', error_val=MNRMSE)
             model.params = add_or_modify_error(dataset_path=dataset_path, params=model.params, error_ref='MNMAE', error_val=MNMAE)
@@ -88,13 +94,13 @@ def eval_error(lpth, input,dataset_path,type,ref, verbose):
 
 
     fig,ax = plt.subplots(1,2)
-    ax[0].bar(range(len(dict_mse)), list(dict_mse.values()),yerr=dict_std_mse.values(), align='center', capsize=5)
+    ax[0].bar(range(len(dict_mse)), list(dict_mse.values()), align='center', capsize=5)
     ax[0].set_xticks(ticks = range(len(dict_mse)))
     ax[0].set_xticklabels(list(dict_mse.keys()))
     ax[0].set_title('Mean NRMSE')
 
 
-    ax[1].bar(range(len(dict_mae)), list(dict_mae.values()),yerr=dict_std_mae.values(), align='center', capsize=5)
+    ax[1].bar(range(len(dict_mae)), list(dict_mae.values()), align='center', capsize=5)
     ax[1].set_xticks(ticks = range(len(dict_mae)))
     ax[1].set_xticklabels(list(dict_mae.keys()))
     ax[1].set_title('Mean NMAE')
@@ -147,7 +153,7 @@ def eval_plot(lpth, input, n, dataset_path, type,merged, ref, verbose, param_com
 
         # print(test_dataset.numpy_cpu_dataset.dtype)
 
-        test_dataloader = DataLoader(dataset=test_dataset,batch_size=params['test_batchsize'],shuffle=False)
+        test_dataloader = DataLoader(dataset=test_dataset,batch_size=32,shuffle=False)
 
         if pth_id==0:
             N_data = len(test_dataloader.dataset)
