1ced62ca033b522c939b492600d115ee05b86935
TheoKaprel <theo.kaprelian@creatis.insa-lyon.fr>
2023-02-23 16:14:49 +0100

    debug hyper param tuning if conditions

--- hyper_param_tuning.py
+++ hyper_param_tuning.py
@@ -47,7 +47,7 @@ def objective_w_params(single_trial, params, tune):
     for param_to_tune in tune:
         if param_to_tune=='data_normalisation':
             params['data_normalisation'] = trial.suggest_categorical('data_normalisation', choices = ["none", "img_mean"])
-        if param_to_tune=='learning_rate':
+        elif param_to_tune=='learning_rate':
             params['learning_rate']=trial.suggest_loguniform('learning_rate', 1e-5, 1e-2)
         elif param_to_tune=='conv3d':
             params['conv3d']=trial.suggest_categorical('conv3d', choices=[True, False])
