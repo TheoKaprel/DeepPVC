dcfa14469d03629dbeb715efb40ad361d60dabce
TheoKaprel <theo.kaprelian@creatis.insa-lyon.fr>
2023-03-09 15:25:16 +0100

    updatable batchsizes

--- DeepPVC/Models.py
+++ DeepPVC/Models.py
@@ -112,11 +112,6 @@ class ModelBase(torch.nn.Module):
         pass
 
     def set_requires_grad(self, nets, requires_grad=False):
-        """Set requies_grad=Fasle for all the networks to avoid unnecessary computations
-        Parameters:
-            nets (network list)   -- a list of networks
-            requires_grad (bool)  -- whether the networks require gradients or not
-        """
         if not isinstance(nets, list):
             nets = [nets]
         for net in nets:
